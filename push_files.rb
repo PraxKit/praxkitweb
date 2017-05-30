fce8dd29b035018cf170cd4c7d48b726d14d4b90
zoosky <zoosky@gmail.com>
2017-05-30 08:59:51 +0200

    change to ruby

---
+++ push_files.rb
@@ -0,0 +1,10 @@
+#!/usr/bin/ruby
+require './ftp_sync.rb'
+
+ftp_server = ENV['FTP_TARGET'] 
+ftp_user = ENV['FTP_USER']
+ftp_password = ENV['FTP_PASSWORD']
+
+print("\n #{ftp_server}\n")
+ftp = FtpSync.new(ftp_server, ftp_user, ftp_password)
+ftp.sync('.', '/')
