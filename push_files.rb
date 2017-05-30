6f2551a7fb78b5e44c8b03b66e689ee89ed5e2dc
zoosky <zoosky@gmail.com>
2017-05-30 11:20:41 +0200

    set passive true

--- push_files.rb
+++ push_files.rb
@@ -6,5 +6,5 @@ ftp_user = ENV['FTP_USER']
 ftp_password = ENV['FTP_PASSWORD']
 
 print("\n #{ftp_server}\n")
-ftp = FtpSync.new(ftp_server, ftp_user, ftp_password)
+ftp = FtpSync.new(ftp_server, ftp_user, ftp_password, true)
 ftp.sync('.', '/')
